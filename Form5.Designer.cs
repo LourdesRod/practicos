5937fdead4705ae36b77c3efb27534333b7ddb58
LourdesRod <lourd.rodoj@gmail.com>
2023-08-28 11:21:57 -0300

    p5

---
+++ Form5.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Practico4
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            button2 = new Button();
+            button1 = new Button();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label2 = new Label();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(579, 72);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(150, 144);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(radioButton2);
+            panel1.Controls.Add(radioButton1);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(dateTimePicker1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Font = new Font("Papyrus", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            panel1.ForeColor = SystemColors.ControlLightLight;
+            panel1.Location = new Point(72, 43);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(446, 365);
+            panel1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.ControlText;
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(160, 308);
+            button2.Name = "button2";
+            button2.Size = new Size(114, 57);
+            button2.TabIndex = 13;
+            button2.Text = "Agregar";
+            button2.TextAlign = ContentAlignment.MiddleRight;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe Fluent Icons", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(31, 253);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 12;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(175, 256);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(150, 58);
+            textBox4.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(175, 206);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(150, 58);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(178, 80);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 58);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(177, 43);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 58);
+            textBox1.TabIndex = 8;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(288, 156);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(241, 55);
+            radioButton2.TabIndex = 7;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(141, 156);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(241, 55);
+            radioButton1.TabIndex = 6;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(41, 204);
+            label5.Name = "label5";
+            label5.Size = new Size(102, 51);
+            label5.TabIndex = 5;
+            label5.Text = "label5";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(45, 156);
+            label4.Name = "label4";
+            label4.Size = new Size(103, 51);
+            label4.TabIndex = 4;
+            label4.Text = "label4";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(42, 127);
+            label3.Name = "label3";
+            label3.Size = new Size(102, 51);
+            label3.TabIndex = 3;
+            label3.Text = "label3";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(183, 115);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(135, 58);
+            dateTimePicker1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(40, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 51);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(31, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 51);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            Name = "Form2";
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private DateTimePicker dateTimePicker1;
+        private Label label2;
+        private Label label1;
+    }
+}
